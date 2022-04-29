f0f583ef6993191442720eb54bf46727814a5659
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-29 09:54:04 -0300

    Problemas para exportar el navbar

--- first-react/src/components/navbar.js
+++ first-react/src/components/navbar.js
@@ -1,10 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Footer.css";
 
-function Navbar() {
+function Footer() {
   return (
-    <h1>Hola soy el Navbar</h1>
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the adventure newletter to receive our best vaction deals
+        </p>
+        <p className="footer-subscription-text">
+          {" "}
+          You can unsuscribe at any time{" "}
+        </p>
+        <div className="input-area">
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your mail"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Suscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-links-items">
+            <h2>About us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL c 2020 </small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"/>
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram"/>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
-export default Navbar;
+export default Footer;
