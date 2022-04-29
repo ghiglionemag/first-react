2bd35a4d0e8294ab906d920f764685d86cdd84fe
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-29 09:51:24 -0300

    Problemas para exportar el navbar

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Footer from '../src/components/Footer';
+import Footer from "../src/components/Footer";
 import Home from "../src/components/pages/Home";
 import Services from "../src/components/pages/Services";
 import Products from "../src/components/pages/Products";
 import SignUp from "../src/components/pages/SignUp";
-import Navbar from '../src/components/Navbar';
-
+import Navbar from "../src/components/Navbar";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path='/' exact element={<Home/>} />
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/products' element={<Products/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-        </Routes>
         <Navbar></Navbar>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
         <Footer></Footer>
       </Router>
     </>
--- first-react/src/components/Footer.js
+++ first-react/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import "./Footer.css";
 
 function Footer() {
   return (
@@ -22,6 +24,46 @@ function Footer() {
             />
             <Button buttonStyle="btn--outline">Suscribe</Button>
           </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-links-items">
+            <h2>About us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL c 2020 </small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"/>
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram"/>
+            </Link>
+          </div>
         </div>
       </section>
     </div>
