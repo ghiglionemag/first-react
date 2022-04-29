6ad9604aefe3dc239be3a7b744cfa57679bf22a0
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-29 09:52:37 -0300

    Problemas para exportar el navbar

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
 import "./App.css";
 import Footer from "../src/components/Footer";
 import Home from "../src/components/pages/Home";
 import Services from "../src/components/pages/Services";
 import Products from "../src/components/pages/Products";
 import SignUp from "../src/components/pages/SignUp";
-import Navbar from "../src/components/Navbar";
 
 function App() {
   return (
