82d6e22c58db369f86e33378d8c9bb2d1fc6f1ab
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-29 20:40:43 -0300

    Volviendo a hacer la navbar

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -6,11 +6,13 @@ import Home from "../src/components/pages/Home";
 import Services from "../src/components/pages/Services";
 import Products from "../src/components/pages/Products";
 import SignUp from "../src/components/pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar></Navbar>
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/services" element={<Services />} />
