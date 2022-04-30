8e4390a4c4afd1d02f8811d146baa674a7f70fbf
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-29 21:48:25 -0300

    Volviendo a hacer la navbar

--- first-react/src/components/Navbar.js
+++ first-react/src/components/Navbar.js
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-// import "./Navbar.css"
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-
-    return (
-      <>
-        <h1>Hola</h1>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div className="navbar"></div>
+    </>
+  );
+}
 
 export default Navbar;
